ba1a2a8989cdded63a197c2223315cbd23e284d5
selbowgreaser <selbowgreaser@gmail.com>
2024-09-27 13:39:31 +0300

    Added init project

---
+++ src/main/java/ru/onenode/teamcitydemoproject/controller/HelloController.java
@@ -0,0 +1,15 @@
+package ru.onenode.teamcitydemoproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/ru/onenode/teamcitydemoproject/TeamcityDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.onenode.teamcitydemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
